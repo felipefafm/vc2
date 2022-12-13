712c28d3db71e8cc5f9323fa16b9b4edd4459c7e
felipefafm <felipealbertofloresm@gmail.com>
2022-12-13 19:41:01 -0300

    spinner

--- src/App.js
+++ src/App.js
@@ -3,13 +3,27 @@ import "./App.css";
 import ItemDetailContainer from "./components/ItemDetailContainer/ItemDetailContainer";
 import ItemListContainer from "./components/ItemListContainer/ItemListContainer";
 import CollapsNav from "./components/Navbar/Navbar";
+import { useEffect,useState } from "react";
+import { ClipLoader } from "react-spinners";
 
 function App() {
+  const[loading, setLoading]=useState(false)
+    useEffect(()=>{
+      setLoading(true)
+      setTimeout(()=>{
+        setLoading(false)
+      },4000)
+    },[])
   return (
     <div className="App">
+     
       <BrowserRouter>
         <CollapsNav />
         <div>
+           {
+        loading?
+        <ClipLoader color={'#D0021B'} loading={loading} size={100} />
+       : 
           <Routes>
             <Route path="/" element={<ItemListContainer />} />
             <Route
@@ -21,6 +35,7 @@ function App() {
               element={<ItemDetailContainer />}
             />
           </Routes>
+      }
         </div>
       </BrowserRouter>
     </div>
--- src/components/ItemDetailContainer/ItemDetailContainer.js
+++ src/components/ItemDetailContainer/ItemDetailContainer.js
@@ -3,12 +3,11 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { getProductById } from "../../asynMock";
 import ItemDetail from "../Itemdetail/ItemDetail";
-// import Loader from "../Loader/Loader";
+
 const ItemDetailContainer = () => {
   const [product, setProduct] = useState({});
   const { productId } = useParams();
-  // const [Loader, setLoader] = useState (true) 
-
+ 
 
 
   useEffect(() => {
@@ -23,7 +22,7 @@ const ItemDetailContainer = () => {
 // console.log(productId)
   return (
     <div>
-      {/* <Loader></Loader> */}
+  
       <h1>Detalle del producto</h1>
       <ItemDetail {...product} />
       
